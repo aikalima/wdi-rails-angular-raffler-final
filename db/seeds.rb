6c6ad4ce7ffbe82a1b1ef5eef8f906cb9986932e
markusg <mguehrs@gmail.com>
2013-08-22 09:33:57 -0700

    init

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+Entry.create!(name: "Lauren")
+Entry.create!(name: "Karl")
+Entry.create!(name: "Lior")
+Entry.create!(name: "Donielle")
+Entry.create!(name: "Ricky")
+Entry.create!(name: "Xiao")
+Entry.create!(name: "Ian")
+Entry.create!(name: "Dave")
+Entry.create!(name: "Brian")
+Entry.create!(name: "Sharif")
+Entry.create!(name: "Matt")
+Entry.create!(name: "Todd")
+Entry.create!(name: "Ryan")
+Entry.create!(name: "Alex")
+Entry.create!(name: "Josh")
+Entry.create!(name: "Justin")
+
+
+
